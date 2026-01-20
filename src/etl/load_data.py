e77e647360b552bee65e7232f9f9e277d7bf38f8
Ola Idczak <olaa.idczak@gmail.com>
2026-01-20 20:43:31 +0100

    fix typo

--- src/etl/load_data.py
+++ src/etl/load_data.py
@@ -4,7 +4,7 @@ import psycopg2
 import psycopg2.extras
 from .get_bike_data import get_and_transform_bike_data
 from .get_weather_data import get_weather_data
-from .get_forcast_data import get_forecast_data
+from .get_forecast_data import get_forecast_data
 
 load_dotenv()
 
