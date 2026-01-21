0b58c587305880b9dad8483d8fec6b0e299403e9
Ola Idczak <olaa.idczak@gmail.com>
2026-01-21 20:59:38 +0100

    add model

--- src/model/model_predict.py
+++ src/model/model_predict.py
@@ -4,7 +4,7 @@ def predict(data):
     with open("model.pkl", "rb") as f:
         model = pickle.load(f)
     
-    data = data[['temperature', 'relative_humidity', 'is_day', 'pressure_msl',
+    data = data[['temperature', 'relative_humidity', 'is_day', 'surface_pressure',
        'snowfall', 'cloud_cover', 'wind_speed', 'wind_direction', 'day',
        'hour']]
     predictions = model.predict(data)
