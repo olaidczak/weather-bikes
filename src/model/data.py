56767a1fea401c106f8b8ad071884329b645069f
Ola Idczak <olaa.idczak@gmail.com>
2026-01-21 14:36:34 +0100

    model outline

--- src/model/data.py
+++ src/model/data.py
@@ -17,17 +17,6 @@ def get_data():
     )
 
     cur = connection.cursor()
-    # query = """
-    #     SELECT
-    #     batch_id,
-    #     ROUND(
-    #         (SUM(free_bikes + empty_slots) - SUM(free_bikes))/(SUM(free_bikes + empty_slots)),
-    #         4
-    #     ) AS used_bikes
-    #     FROM bike_stations_status
-    #     GROUP BY batch_id
-    #     ORDER BY batch_id;
-    # """
 
     query = """
         SELECT
@@ -49,6 +38,35 @@ def get_data():
         ORDER BY bss.batch_id;
     """
 
+    cur.execute(query)
+
+    rows = cur.fetchall()
+    colnames = [desc[0] for desc in cur.description]
+
+    df = pd.DataFrame(rows, columns=colnames)
+
+    cur.close()
+    connection.close()
+    return df
+
+
+def get_forecast_data():
+    connection = psycopg2.connect(
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        database=os.getenv("DB_NAME"),
+        sslmode=os.getenv("DB_SSLMODE"),
+    )
+
+    cur = connection.cursor()
+
+    query = """
+        SELECT temperature, relative_humidity, is_day, surface_pressure,
+        snowfall, cloud_cover, wind_speed, wind_direction, day, hour 
+        from forecast;
+    """
     cur.execute(query)
 
     rows = cur.fetchall()
