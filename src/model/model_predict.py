b3ca5fd3fc40dd74181d1667a3d4c1b378c87e22
Ola Idczak <olaa.idczak@gmail.com>
2026-01-21 14:36:34 +0100

    model outline

---
+++ src/model/model_predict.py
@@ -0,0 +1,11 @@
+import pickle
+
+def predict(data):
+    with open("model.pkl", "rb") as f:
+        model = pickle.load(f)
+    
+    data = data[['temperature', 'relative_humidity', 'is_day', 'pressure_msl',
+       'snowfall', 'cloud_cover', 'wind_speed', 'wind_direction', 'day',
+       'hour']]
+    predictions = model.predict(data)
+    return predictions
